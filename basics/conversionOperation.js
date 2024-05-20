d5298ab2d7fe7a0b8882324c0ec77cda89f703ef
bhardwajvishal0893 <bhardwajvishal0893@gmail.com>
2024-05-20 19:52:02 +0000

    additionaconversion

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -25,3 +25,32 @@ let isLoggedIn = 1;
 let boolIsLoggedIn = Boolean(isLoggedIn);
 console.log(boolIsLoggedIn);
 // 1=>true 0=>false ""=>false "Vishal"=>true
+
+
+let value = 5;
+let negValue = -value;
+console.log(negValue);
+console.log(2+2);
+console.log(2*2);
+console.log(2**2);//power
+console.log(2%2);
+
+let str1 = "Vishal"
+let str2 = " Bhardwaj"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); //32
+
+console.log(true);//will return true output
+console.log(+true); //will return 1 output
+console.log(+""); // 0 will be output
+
+let a=10;
+console.log(a++); // resulting output is 10
+let b=10
+console.log(++b); //resulting output is 11
